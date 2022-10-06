3119a239b3e28faf719ea8997818cfa29e97cf88
xulinkai <2509513813@qq.com>
2022-10-06 19:57:42 +0800

    xulinkai1006:Replace a piece of code in css with  js implementation

---
+++ js/test.js
@@ -0,0 +1,22 @@
+var ab=document.getElementsByClassName('top');
+for(var i=0;i<ab.length;i++){
+    ab[i].onmouseover=function(){
+    this.style.backgroundColor='#999';
+    
+}
+}
+for(var i=0;i<ab.length;i++){
+ab[i].onmouseleave=function(){
+    this.style.backgroundColor='transparent';
+}}
+var ab1=document.getElementsByClassName('top1');
+for(var i=0;i<ab1.length;i++){
+    ab1[i].onmouseover=function(){
+    this.style.backgroundColor='#999';
+    
+}
+}
+for(var i=0;i<ab1.length;i++){
+ab1[i].onmouseleave=function(){
+    this.style.backgroundColor='transparent';
+}}
